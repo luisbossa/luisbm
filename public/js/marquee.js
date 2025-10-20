669849915305e13a92496e6c5aac123857008d11
luisbossa <luisbossa@gmail.com>
2025-10-20 00:34:56 -0600

    add new project

--- public/js/marquee.js
+++ public/js/marquee.js
@@ -3,8 +3,9 @@ const video2 = document.getElementById("projectVideo2");
 const video3 = document.getElementById("projectVideo3");
 const video4 = document.getElementById("projectVideo4");
 const video5 = document.getElementById("projectVideo5");
+const video6 = document.getElementById("projectVideo6");
 
-const videoList = [video1, video2, video3, video4, video5];
+const videoList = [video1, video2, video3, video4, video5, video6];
 
 videoList.forEach(function (video) {
   video.addEventListener("mouseover", function () {
