656d4821afe59c5761c114f662731c1876849a5a
luisbossa <luisbossa@gmail.com>
2025-11-17 01:17:33 -0600

    js

--- public/js/marquee.js
+++ public/js/marquee.js
@@ -1,13 +1,6 @@
 const videos = document.querySelectorAll("video");
 
 videos.forEach((video) => {
-  const wrapper = video.parentElement;
-  const placeholder = wrapper.querySelector(".video-placeholder");
-
-  video.addEventListener("loadeddata", () => {
-    placeholder.classList.add("hidden");
-  });
-
   video.addEventListener("mouseover", () => video.play());
   video.addEventListener("mouseout", () => video.pause());
 });
