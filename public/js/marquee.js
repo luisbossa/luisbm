6aef1596861b6551960ae847c019d1374f1a7916
luisbossa <luisbossa@gmail.com>
2025-11-16 19:31:37 -0600

    better responsive

--- public/js/marquee.js
+++ public/js/marquee.js
@@ -1,17 +1,13 @@
-const video1 = document.getElementById("projectVideo1");
-const video2 = document.getElementById("projectVideo2");
-const video3 = document.getElementById("projectVideo3");
-const video4 = document.getElementById("projectVideo4");
-const video5 = document.getElementById("projectVideo5");
-const video6 = document.getElementById("projectVideo6");
+const videos = document.querySelectorAll("video");
 
-const videoList = [video1, video2, video3, video4, video5, video6];
+videos.forEach((video) => {
+  const wrapper = video.parentElement;
+  const placeholder = wrapper.querySelector(".video-placeholder");
 
-videoList.forEach(function (video) {
-  video.addEventListener("mouseover", function () {
-    video.play();
+  video.addEventListener("loadeddata", () => {
+    placeholder.classList.add("hidden");
   });
-  video.addEventListener("mouseout", function () {
-    video.pause();
-  });
+
+  video.addEventListener("mouseover", () => video.play());
+  video.addEventListener("mouseout", () => video.pause());
 });
